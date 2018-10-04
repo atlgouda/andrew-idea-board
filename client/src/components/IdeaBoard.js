39d2320a3b7d39f024ec14d985e89fbd710d4d0b
Gouda <gouda.clark@gmail.com>
2018-10-04 15:36:52 -0400

    styled sticky notes

--- client/src/components/IdeaBoard.js
+++ client/src/components/IdeaBoard.js
@@ -1,6 +1,33 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+//npm i styled-components under client
+import styled from 'styled-components'
 
+//MAKE SURE TO USE BACKTICKS
+const StyledIdea = styled.div`
+  background-color: rgb(255, 255, 136);
+  border: 1px solid black;
+  width: 30vw;
+  min-width: 100px;
+  max-width: 200px;
+  padding: 5%;
+  margin: 10vh;
+  min-height: 26vh;
+  `
+const StyledTitle = styled.div`
+  text-align: center;
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  font-weight: bold;
+`
+const StyledIdeaTitle = styled.div`
+  border-bottom: 1px solid black;
+  margin-bottom: 5vh;
+  `
+const StyledDescription = styled.div`
+  border: 1px double blue;
+  padding: 6px;
+`
+  
 export default class IdeaBoard extends Component {
   state = {
     user: {},
@@ -25,17 +52,18 @@ export default class IdeaBoard extends Component {
   render() {
     const ideasList = this.state.ideas.map( idea => {
       return (
-        <div key={idea._id}>
-      <div>{idea.title}</div>
-      <div>{idea.description}</div>
-      </div>
+    //CHANGE DIVS TO STYLED DIVS FOR WHERE YOU WANT TO STYLE
+        <StyledIdea key={idea._id}>
+      <StyledIdeaTitle>{idea.title}</StyledIdeaTitle>
+      <StyledDescription>{idea.description}</StyledDescription>
+      </StyledIdea>
       )
     })
     return (
-      <div>
+      <StyledTitle>
         <h1>Idea Board for {this.state.user.userName}</h1>
       {ideasList}
-      </div>
+      </StyledTitle>
     )
   }
 }
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
 export default class Login extends Component {
   state = {
